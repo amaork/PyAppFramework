92de37648fbff4915a99fd9c16907765a450a033
wxidong@Sanai.win7 <amaork@gmail.com>
2016-10-18 10:54:22 +0800

    core.datatype add DynamicObject base class

--- core/datatype.py
+++ core/datatype.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import json
 import types
 import ctypes
 
 
-__all__ = ['BasicDataType', 'BasicTypeLE', 'BasicTypeBE',
+__all__ = ['BasicDataType', 'BasicTypeLE', 'BasicTypeBE', 'DynamicObject',
            'str2float', 'str2number',
            'new_class', 'new_instance',
            'ip4_check']
@@ -124,7 +125,7 @@ class BasicDataType(object):
 
 class BasicTypeLE(BasicDataType, ctypes.LittleEndianStructure):
     def __eq__(self, other):
-        if not isinstance(other, BasicTypeLE):
+        if not isinstance(other, self.__class__):
             return False
         return self.cdata() == other.cdata()
 
@@ -134,9 +135,71 @@ class BasicTypeLE(BasicDataType, ctypes.LittleEndianStructure):
 
 class BasicTypeBE(BasicDataType, ctypes.BigEndianStructure):
     def __eq__(self, other):
-        if not isinstance(other, BasicTypeBE):
+        if not isinstance(other, self.__class__):
             return False
         return self.cdata() == other.cdata()
 
     def __ne__(self, other):
         return not self.__eq__(other)
+
+
+class DynamicObject(object):
+    _properties = ()
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(**kwargs)
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __len__(self):
+        return len(self._properties)
+
+    def __str__(self):
+        return self.encode()
+
+    def __iter__(self):
+        for key in sorted(self.__dict__.keys()):
+            yield key
+
+    def __getattr__(self, name):
+        try:
+            return self.__dict__[name]
+        except KeyError:
+            msg = "'{0}' object has no attribute '{1}'"
+            raise AttributeError(msg.format(type(self).__name__, name))
+
+    def decode(self, data):
+        """Decode data
+
+        :param data: encoded data, should be a str
+        :return: success return data dict else NONE
+        """
+        if not isinstance(data, str):
+            return None
+
+        try:
+
+            dict_ = json.loads(data)
+            if not isinstance(dict_, dict):
+                return None
+
+            for key in self._properties:
+                if dict_.get(key) is None:
+                    return None
+
+            return dict_
+
+        except ValueError, e:
+            print "Decode object '{0}' has error:{1}".format(type(self), e)
+            return None
+
+    def encode(self):
+        """Encode data to a dict string
+
+        :return:
+        """
+        return json.dumps(self.__dict__)
