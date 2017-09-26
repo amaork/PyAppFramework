4cd56ac8b1c82a59a7c628ce69fbc1ad9ca9bb6f
wxidong@Sanai.win7 <amaork@gmail.com>
2017-09-26 14:32:14 +0800

    refactor misc.process module add ProcessManager class

--- misc/process.py
+++ misc/process.py
@@ -3,74 +3,115 @@ import os
 import signal
 import platform
 
-_system = platform.system().lower()
-
-__all__ = ['get_pid', 'process_start', 'process_stop', 'process_kill']
+__all__ = ['ProcessManager']
 
 
-def get_pid(name):
-    """Get process id
+class ProcessManager(object):
+    SYSTEM = platform.system().lower()
 
-    :param name: process name
-    :return: pid list
-    """
-    if _system == "windows":
-        cmd = os.popen("tasklist | grep {0:s} | awk '{{ print $2 }}'".format(name))
-    else:
-        cmd = os.popen('pidof {0:s}'.format(name))
+    def __init__(self, name, cmdline, autostart=True):
+        """Process manager
 
-    data = cmd.read().strip()
-    if not data:
-        return []
-    return [int(pid) for pid in data.split(' ')]
+        :param name: name
+        :param cmdline: command line
+        :param autostart: if set auto start program
+        """
+        self.__name = name
+        self.__cmdline = cmdline
+        if autostart:
+            self.resume()
 
+    def __process_control(self, run):
+        """Process control
 
-def process_control(name, cmd, start):
-    """Control process
+        :param run:
+        :return:
+        """
+        retry = 3
+        lst = list()
+        name = self.name()
+        cmd = os.path.basename(self.cmdline())
+        path = os.path.dirname(self.cmdline())
 
-    :param name: process name for get_pid
-    :param cmd: process start command
-    :param start: True start, False stop
-    :return:
-    """
-    retry = 3
-    lst = list()
-    assert isinstance(cmd, str), 'process cmd error!'
+        try:
 
-    # Make program running in background
-    if _system == "windows":
-        cmd = "start /b {0:s}".format(cmd)
-    else:
-        if not cmd.endswith('&'):
-            cmd += ' &'
+            if os.path.isdir(path):
+                os.chdir(path)
 
-    while not lst and retry > 0:
-        lst = get_pid(name)
-        if not lst:
-            os.system(cmd)
-            retry -= 1
-            continue
+            # Make program running in background
+            if self.is_windows():
+                cmd = "start /b {0:s}".format(cmd)
+            else:
+                if not cmd.endswith('&'):
+                    cmd += ' &'
+                cmd = "./{}".format(cmd)
 
-        if _system != "windows":
-            for pid in lst:
-                os.kill(pid, signal.SIGCONT if start else signal.SIGSTOP)
+            while not lst and retry > 0:
+                lst = self.get_pid(name)
+                if not lst:
+                    os.system(cmd)
+                    retry -= 1
+                    continue
 
+                if not self.is_windows():
+                    for pid in lst:
+                        os.kill(pid, signal.SIGCONT if run else signal.SIGSTOP)
+        except StandardError:
+            pass
 
-def process_start(name, cmd):
-    process_control(name, cmd, True)
-    return True if get_pid(name) else False
+    def name(self):
+        return self.__name
 
+    def cmdline(self):
+        return self.__cmdline
 
-def process_stop(name, cmd):
-    if _system == "windows":
-        return process_kill(name)
-    process_control(name, cmd, False)
-    return True if get_pid(name) else False
+    @staticmethod
+    def is_windows():
+        return ProcessManager.SYSTEM == "windows"
 
+    @staticmethod
+    def get_pid(name):
+        """Get process id
 
-def process_kill(name):
-    kill_signal = signal.SIGILL if _system == "windows" else signal.SIGKILL
-    for pid in get_pid(name):
-        os.kill(pid, kill_signal)
+           :param name: process name
+           :return: pid list
+           """
+        if ProcessManager.is_windows():
+            cmd = os.popen("tasklist | grep {0:s} | awk '{{ print $2 }}'".format(name))
+        else:
+            cmd = os.popen('pidof {0:s}'.format(name))
 
-    return True if not get_pid(name) else False
+        data = cmd.read().strip()
+        if not data:
+            return []
+        return [int(pid) for pid in data.split(' ')]
+
+    @staticmethod
+    def kill(name):
+        """Kill name specified process
+
+        :param name: process name
+        :return: success return True, failed return false
+        """
+        kill_signal = signal.SIGILL if ProcessManager.is_windows() else signal.SIGKILL
+        for pid in ProcessManager.get_pid(name):
+            os.kill(pid, kill_signal)
+
+        return True if not ProcessManager.get_pid(name) else False
+
+    def suspend(self):
+        if self.is_windows():
+            return self.kill(self.name())
+
+        self.__process_control(False)
+        return True if self.get_pid(self.name()) else False
+
+    def resume(self):
+        self.__process_control(True)
+        return True if self.get_pid(self.name()) else False
+
+    def terminate(self):
+        return self.kill(self.name())
+
+    def is_running(self):
+        return len(self.get_pid(self.name())) != 0
