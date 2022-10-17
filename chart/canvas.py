9cc3aa9bfcf62cbb070c253f6039d7e3ed61699e
amaork@Home_WIN7 <amaork@gmail.com>
2022-10-17 22:56:34 +0800

    move ChartAxesAttribute from chart.line to chart.canvas

--- chart/canvas.py
+++ chart/canvas.py
@@ -3,8 +3,25 @@ import contextlib
 import matplotlib.axes
 matplotlib.use('Qt5Agg')
 from matplotlib.figure import Figure
+from ..core.datatype import DynamicObject
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-__all__ = ['CustomCanvas']
+__all__ = ['CustomCanvas', 'ChartAxesAttribute']
+
+
+class ChartAxesAttribute(DynamicObject):
+    _properties = {'lines', 'title_kwargs',
+                   'show_frame', 'show_grid',
+                   'y_ticks', 'x_ticks', 'x_label', 'y_label', 'legend_kwargs'}
+
+    def __init__(self, **kwargs):
+        kwargs.setdefault('lines', [])
+        kwargs.setdefault('x_ticks', [])
+        kwargs.setdefault('x_label', '')
+        kwargs.setdefault('y_label', '')
+        kwargs.setdefault('show_grid', True)
+        kwargs.setdefault('show_frame', True)
+        kwargs.setdefault('legend_kwargs', None)
+        super(ChartAxesAttribute, self).__init__(**kwargs)
 
 
 class CustomCanvas(FigureCanvas):
@@ -20,3 +37,27 @@ class CustomCanvas(FigureCanvas):
         self.axes.cla()
         yield
         self.draw()
+
+    def updateAxesAttribute(self, attribute: ChartAxesAttribute):
+        if attribute.legend_kwargs:
+            self.axes.legend(**attribute.legend_kwargs)
+
+        if attribute.x_label:
+            self.axes.set_xlabel(attribute.x_label)
+
+        if attribute.y_label:
+            self.axes.set_ylabel(attribute.y_label)
+
+        if attribute.x_ticks:
+            self.axes.set_xticks(attribute.x_ticks)
+
+        if attribute.y_ticks:
+            self.axes.set_yticks(attribute.y_ticks)
+
+        if attribute.title_kwargs:
+            self.axes.set_title(**attribute.title_kwargs)
+
+        self.axes.set_frame_on(attribute.show_frame)
+
+        if attribute.show_grid:
+            self.axes.grid()
--- chart/line.py
+++ chart/line.py
@@ -4,26 +4,10 @@ import matplotlib.axes
 matplotlib.use('Qt5Agg')
 from PySide2 import QtWidgets
 
-from .canvas import CustomCanvas
 from ..gui.widget import BasicWidget
-from ..core.datatype import DynamicObject
-__all__ = ['TimelineChartView', 'ChartLine', 'ChartAxesAttribute']
+from .canvas import CustomCanvas, ChartAxesAttribute
+__all__ = ['TimelineChartView', 'ChartLine']
 ChartLine = collections.namedtuple('ChartLine', 'name tag color max')
-
-
-class ChartAxesAttribute(DynamicObject):
-    _properties = {'lines', 'title_kwargs',
-                   'show_frame', 'show_grid',
-                   'y_ticks', 'x_ticks', 'x_label', 'y_label', 'legend_kwargs'}
-
-    def __init__(self, **kwargs):
-        kwargs.setdefault('x_ticks', [])
-        kwargs.setdefault('x_label', '')
-        kwargs.setdefault('y_label', '')
-        kwargs.setdefault('show_grid', True)
-        kwargs.setdefault('show_frame', True)
-        kwargs.setdefault('legend_kwargs', None)
-        super(ChartAxesAttribute, self).__init__(**kwargs)
 
 
 class TimelineChartView(BasicWidget):
@@ -43,13 +27,7 @@ class TimelineChartView(BasicWidget):
         self.xdata = list()
         self.ydata = {item.tag: list() for item in self._attribute.lines}
 
-    def _initStyle(self):
-        pass
-
-    def _initThreadAndTimer(self):
-        pass
-
-    def slotUpdatePlot(self, data: dict):
+    def updateChart(self, data: dict):
         self.cnt += 1
         self.xdata.append(self.cnt)
 
@@ -58,29 +36,4 @@ class TimelineChartView(BasicWidget):
                 self.ydata[line.tag].append(data.get(line.tag))
                 self.canvas.axes.plot(self.xdata, self.ydata[line.tag], line.color, label=line.name)
 
-            self.updateAxes(self.canvas.axes, self._attribute)
-
-    @staticmethod
-    def updateAxes(axes: matplotlib.axes.Axes, attribute: ChartAxesAttribute):
-        if attribute.legend_kwargs:
-            axes.legend(**attribute.legend_kwargs)
-
-        if attribute.x_label:
-            axes.set_xlabel(attribute.x_label)
-
-        if attribute.y_label:
-            axes.set_ylabel(attribute.y_label)
-
-        if attribute.x_ticks:
-            axes.set_xticks(attribute.x_ticks)
-
-        if attribute.y_ticks:
-            axes.set_yticks(attribute.y_ticks)
-
-        if attribute.title_kwargs:
-            axes.set_title(**attribute.title_kwargs)
-
-        axes.set_frame_on(attribute.show_frame)
-
-        if attribute.show_grid:
-            axes.grid()
+            self.canvas.updateAxesAttribute(self._attribute)
