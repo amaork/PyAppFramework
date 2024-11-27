53fcef63b361f2cee78cb41c5aea53657275707a
amaork@Home_WIN10 <amaork@gmail.com>
2024-11-27 11:36:41 +0800

    fix gui.msgbox show extra buttons cause standard buttons disappear bugs

--- gui/msgbox.py
+++ gui/msgbox.py
@@ -20,6 +20,13 @@ def addExtraButtonsToMsgBox(msg_box: QMessageBox, extra_buttons: typing.Sequence
             msg_box.addButton(btn, QMessageBox.ButtonRole.YesRole)
 
 
+def delExtraButtonsToMsgBox(msg_box: QMessageBox, extra_buttons: typing.Sequence[QPushButton] = None):
+    extra_buttons = extra_buttons or list()
+    if extra_buttons and all(isinstance(x, QPushButton) for x in extra_buttons):
+        for btn in extra_buttons:
+            msg_box.removeButton(btn)
+
+
 def showQuestionBox(parent: QWidget, content: str, title: str = '') -> bool:
     """ Show a Question message box and return result
 
@@ -57,6 +64,7 @@ def showMessageBox(parent: QWidget, msg_type: str, content: str,
         buttons = QMessageBox.Ok | QMessageBox.Cancel if msg_type == MB_TYPE_QUESTION else QMessageBox.Ok
         msg_box = QMessageBox(icon, title, content, buttons, parent=parent)
         addExtraButtonsToMsgBox(msg_box, extra_buttons)
+        msg_box.setStandardButtons(buttons)
 
         if msg_type == MB_TYPE_QUESTION:
             return msg_box.exec_() == QMessageBox.Ok
@@ -78,16 +86,17 @@ def showOnSPMsgBox(parent: QWidget, msg_box: QMessageBox, msg_type: str,
         MB_TYPE_QUESTION: (QMessageBox.Question, QApplication.translate("msgbox", "Confirm", None))
     }
 
+    buttons = QMessageBox.Ok | QMessageBox.Cancel if msg_type == MB_TYPE_QUESTION else QMessageBox.Ok
+
     try:
         icon, default_title = __attributes.get(msg_type)
         title = title if title else parent.tr(default_title)
-        buttons = QMessageBox.Ok | QMessageBox.Cancel if msg_type == MB_TYPE_QUESTION else QMessageBox.Ok
 
         msg_box.setIcon(icon)
         msg_box.setText(content)
         msg_box.setWindowTitle(title)
-        msg_box.setDefaultButton(buttons)
         addExtraButtonsToMsgBox(msg_box, extra_buttons)
+        msg_box.setStandardButtons(buttons)
 
         if msg_type == MB_TYPE_QUESTION:
             return msg_box.exec_() == QMessageBox.Ok
@@ -97,3 +106,6 @@ def showOnSPMsgBox(parent: QWidget, msg_box: QMessageBox, msg_type: str,
     except (TypeError, AttributeError) as e:
         print(f'showOnMsgBox:{e}')
         return False
+    finally:
+        delExtraButtonsToMsgBox(msg_box, extra_buttons)
+        msg_box.setStandardButtons(buttons)
